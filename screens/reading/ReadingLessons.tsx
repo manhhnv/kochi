3695adb778e616a3b929dd31b0249dafb6aaa43c
manh117bg@gmail.com <57697334+Nguyen-Van-Manh@users.noreply.github.com>
2020-12-05 12:37:13 +0700

    update effect profile screen

--- screens/reading/ReadingLessons.tsx
+++ screens/reading/ReadingLessons.tsx
@@ -1,18 +1,42 @@
 import { Text, View } from 'native-base';
-import React from 'react';
-import { Dimensions, ImageBackground } from 'react-native';
-
+import React, { useEffect } from 'react';
+import { Animated, Dimensions, ImageBackground, ScrollView, StyleSheet } from 'react-native';
+const { width, height } = Dimensions.get('screen');
 const ReadingLessons = () => {
+    
     return (
         <ImageBackground
             source={require('../../assets/images/background2.jpg')}
             style={{
-                width: Dimensions.get('screen').width,
-                height: Dimensions.get('screen').height
+                width: width,
+                height: 0.9 * height
             }}
         >
+            <ScrollView>
+                <View style={styles.readingCategory}>
 
+                </View>
+            </ScrollView>
         </ImageBackground>
     )
 }
+const styles = StyleSheet.create({
+    readingCategory: {
+        backgroundColor: '#fff',
+        shadowColor: "#000",
+        shadowOffset: {
+            width: 0,
+            height: 5,
+        },
+        shadowOpacity: 0.34,
+        shadowRadius: 6.27,
+        elevation: 10,
+        height: 0.14*height,
+        marginTop: 0.1*height,
+        borderRadius: 15,
+        borderBottomWidth: 5,
+        borderBottomColor: '#00CE9F',
+        marginHorizontal: 0.03*width
+    }
+});
 export default ReadingLessons;
