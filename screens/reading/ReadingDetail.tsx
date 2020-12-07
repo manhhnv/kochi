400081836fa4fb424c713ff1ff8215e0784de5da
manh117bg@gmail.com <57697334+Nguyen-Van-Manh@users.noreply.github.com>
2020-12-07 23:30:03 +0700

    fix font roboto_media not load in system

--- screens/reading/ReadingDetail.tsx
+++ screens/reading/ReadingDetail.tsx
@@ -1,5 +1,11 @@
-import { Button, Content, Radio, Text, View } from 'native-base';
-import React, { useState } from 'react';
+import {
+    Button, Col, Content, Grid, Radio, Row, Text, View,
+    ActionSheet,
+    Container,
+    Root,
+    Icon
+} from 'native-base';
+import React, { useRef, useState } from 'react';
 import { Dimensions, ScrollView, StyleSheet } from 'react-native';
 import * as Animatable from 'react-native-animatable';
 const { width, height } = Dimensions.get('window');
@@ -7,8 +13,14 @@ const ReadingDetail = ({ route, navigation }: any) => {
     console.log("LESSON", route.params.lesson)
     const { lesson } = route.params;
     const [userAnswer, setUserAnswer]: any = useState(null);
+    const [resultCheck, setResultCheck]: any = useState(false);
+    const scrollRef: any = useRef();
+    const showResultCheck = () => {
+        scrollRef.current.scrollTo({x: 0, y: 0, animated: true})
+        setResultCheck(true);
+    }
     return (
-        <ScrollView style={{ backgroundColor: "white" }}>
+        <ScrollView ref={scrollRef} style={{ backgroundColor: "white" }} scrollEnabled={!resultCheck}>
             <Animatable.View animation="fadeIn" style={styles.container} duration={1000}>
                 <Text style={styles.lessonTitle}>
                     {lesson.title}
@@ -17,25 +29,44 @@ const ReadingDetail = ({ route, navigation }: any) => {
                     {lesson.paragraph}
                 </Text>
                 <Text style={styles.lessonDetail}>
-                    <Text style={[{ fontWeight: "bold" }, styles.lessonDetail]}>
+                    <Text style={[{ fontWeight: "bold", color: 'red' }, styles.lessonDetail]}>
                         問題：
                     </Text>
                     {lesson.question}
                 </Text>
                 {lesson.answers.map((answer: any, i: number) => (
                     <View key={i} style={styles.answerRadio}>
-                        <Radio color="gray" selected={userAnswer === i+1} selectedColor="green" onPress={() => setUserAnswer(answer.id)}/>
+                        <Radio color="gray" selected={userAnswer === i + 1} selectedColor="green" onPress={() => setUserAnswer(answer.id)} />
                         <Text style={{ fontSize: 20 }}>{answer.text}</Text>
                     </View>
                 ))}
-                <View style={{flexDirection: "row", alignSelf: "center"}}>
-                    <Button success>
-                        <Text>Đáp án</Text>
-                    </Button>
-                    <Button primary>
-                        <Text>Bài tiếp theo</Text>
-                    </Button>
-                </View>
+                <Grid>
+                    <Root>
+                        <Row style={{ marginTop: 0.1 * width, marginLeft: 0.17 * width, marginBottom: 0.18 * width }}>
+                            <Col>
+                                <Button success onPress={showResultCheck} disabled={userAnswer !== null ? false : true}>
+                                    <Text
+                                    >
+                                        Đáp án
+                                </Text>
+                                </Button>
+                            </Col>
+                            <Col>
+                                <Button primary>
+                                    <Text>Tiếp theo</Text>
+                                </Button>
+                            </Col>
+                        </Row>
+                    </Root>
+                </Grid>
+                {resultCheck == true ? (
+                    <View style={styles.result}>
+                        <Button iconLeft danger onPress={() => setResultCheck(false)}>
+                            <Icon name='close' />
+                            <Text>Close</Text>
+                        </Button>
+                    </View>
+                ): null}
             </Animatable.View>
         </ScrollView>
     )
@@ -48,17 +79,26 @@ const styles = StyleSheet.create({
         fontWeight: "bold",
         textAlign: "center",
         fontSize: 24,
+        color: 'blue'
     },
     lessonDetail: {
         lineHeight: 30,
         padding: 10,
-        fontSize: 20
+        fontSize: 20,
     },
     answerRadio: {
         flexDirection: "row",
         marginTop: 0.01 * height,
         padding: 10,
         marginBottom: 0.01 * height,
+    },
+    result: {
+        height: 0.6*height,
+        backgroundColor: 'green',
+        position: "relative",
+        top: -1.1*height,
+        margin: 15,
+        borderRadius: 15
     }
 })
-export default ReadingDetail;
+export default React.memo(ReadingDetail);
