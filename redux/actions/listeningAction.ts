ad98de10ee49b701f37eebf9e4531229ee6d155e
manh117bg@gmail.com <57697334+Nguyen-Van-Manh@users.noreply.github.com>
2020-12-10 10:38:51 +0700

    get data from API listening

--- redux/actions/listeningAction.ts
+++ redux/actions/listeningAction.ts
@@ -1,4 +1,34 @@
+import { getListeningByCategory } from '../../data/getListeningByCategory';
+
 export const GET_LISTENING_LESSON_TOPIC = "GET_LISTENING_LESSON_TOPIC";
 export const GET_LISTENING_LESSON_CONVERSATION = "GET_LISTENING_LESSON_CONVERSATION";
 export const GET_LISTENING_LESSON_REPLY = "GET_LISTENING_LESSON_REPLY";
 
+export const getLessons = (token: string, category: string, callback?: any) => {
+    return async (dispatch: any) => {
+        const lessons = getListeningByCategory(category);
+        switch(category) {
+            case "topic":
+                dispatch({
+                    type: GET_LISTENING_LESSON_TOPIC,
+                    payload: lessons
+                })
+                break;
+            case "conversation":
+                dispatch({
+                    type: GET_LISTENING_LESSON_CONVERSATION,
+                    payload: lessons
+                })
+            case "reply":
+                dispatch({
+                    type: GET_LISTENING_LESSON_REPLY,
+                    payload: lessons
+                })
+            default:
+                return null;
+        }
+        if (callback) {
+            callback(false)
+        }
+    }
+}
